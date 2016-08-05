b5bcc3eb81621ebcbf056aad73b152dec755c3bd
senator <senator@scryptmail.com>
2016-08-05 06:54:31 +0100

    Master Commit v1
    v1

---
+++ main_window.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace glua_obsfucator
+{
+    partial class main_window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main_window));
+            this.not_ob_text = new System.Windows.Forms.RichTextBox();
+            this.ob_text = new System.Windows.Forms.RichTextBox();
+            this.obsfucate_lua = new System.Windows.Forms.Button();
+            this.obsfucation_status = new System.Windows.Forms.ListBox();
+            this.obs_striplines = new System.Windows.Forms.CheckBox();
+            this.obs_variables = new System.Windows.Forms.CheckBox();
+            this.obs_strings = new System.Windows.Forms.CheckBox();
+            this.obs_hexfunctions = new System.Windows.Forms.CheckBox();
+            this.about = new System.Windows.Forms.Button();
+            this.obs_spacecode = new System.Windows.Forms.CheckBox();
+            this.obs_removecomments = new System.Windows.Forms.CheckBox();
+            this.obs_commentspam = new System.Windows.Forms.CheckBox();
+            this.obs_lowratio = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // not_ob_text
+            // 
+            this.not_ob_text.Dock = System.Windows.Forms.DockStyle.Top;
+            this.not_ob_text.Location = new System.Drawing.Point(0, 0);
+            this.not_ob_text.Name = "not_ob_text";
+            this.not_ob_text.Size = new System.Drawing.Size(507, 150);
+            this.not_ob_text.TabIndex = 0;
+            this.not_ob_text.Text = "Enter Lua Here";
+            // 
+            // ob_text
+            // 
+            this.ob_text.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ob_text.Location = new System.Drawing.Point(0, 150);
+            this.ob_text.Name = "ob_text";
+            this.ob_text.ReadOnly = true;
+            this.ob_text.Size = new System.Drawing.Size(507, 150);
+            this.ob_text.TabIndex = 1;
+            this.ob_text.Text = "";
+            // 
+            // obsfucate_lua
+            // 
+            this.obsfucate_lua.Dock = System.Windows.Forms.DockStyle.Top;
+            this.obsfucate_lua.Location = new System.Drawing.Point(0, 300);
+            this.obsfucate_lua.Name = "obsfucate_lua";
+            this.obsfucate_lua.Size = new System.Drawing.Size(507, 27);
+            this.obsfucate_lua.TabIndex = 2;
+            this.obsfucate_lua.Text = "Obsfucate LUA";
+            this.obsfucate_lua.UseVisualStyleBackColor = true;
+            this.obsfucate_lua.Click += new System.EventHandler(this.obsfucate_lua_Click);
+            // 
+            // obsfucation_status
+            // 
+            this.obsfucation_status.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.obsfucation_status.FormattingEnabled = true;
+            this.obsfucation_status.Location = new System.Drawing.Point(0, 472);
+            this.obsfucation_status.Name = "obsfucation_status";
+            this.obsfucation_status.Size = new System.Drawing.Size(507, 160);
+            this.obsfucation_status.TabIndex = 3;
+            // 
+            // obs_striplines
+            // 
+            this.obs_striplines.AutoSize = true;
+            this.obs_striplines.Location = new System.Drawing.Point(6, 19);
+            this.obs_striplines.Name = "obs_striplines";
+            this.obs_striplines.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.obs_striplines.Size = new System.Drawing.Size(97, 17);
+            this.obs_striplines.TabIndex = 4;
+            this.obs_striplines.Text = "Strip NewLines";
+            this.obs_striplines.UseVisualStyleBackColor = true;
+            // 
+            // obs_variables
+            // 
+            this.obs_variables.AutoSize = true;
+            this.obs_variables.Location = new System.Drawing.Point(214, 65);
+            this.obs_variables.Name = "obs_variables";
+            this.obs_variables.Size = new System.Drawing.Size(121, 17);
+            this.obs_variables.TabIndex = 6;
+            this.obs_variables.Text = "Obsfucate Variables";
+            this.obs_variables.UseVisualStyleBackColor = true;
+            // 
+            // obs_strings
+            // 
+            this.obs_strings.AutoSize = true;
+            this.obs_strings.Location = new System.Drawing.Point(214, 19);
+            this.obs_strings.Name = "obs_strings";
+            this.obs_strings.Size = new System.Drawing.Size(98, 17);
+            this.obs_strings.TabIndex = 8;
+            this.obs_strings.Text = "Encode Strings";
+            this.obs_strings.UseVisualStyleBackColor = true;
+            // 
+            // obs_hexfunctions
+            // 
+            this.obs_hexfunctions.AutoSize = true;
+            this.obs_hexfunctions.Location = new System.Drawing.Point(214, 42);
+            this.obs_hexfunctions.Name = "obs_hexfunctions";
+            this.obs_hexfunctions.Size = new System.Drawing.Size(144, 17);
+            this.obs_hexfunctions.TabIndex = 9;
+            this.obs_hexfunctions.Text = "Obsfucate Funcs/Strings";
+            this.obs_hexfunctions.UseVisualStyleBackColor = true;
+            // 
+            // about
+            // 
+            this.about.Dock = System.Windows.Forms.DockStyle.Top;
+            this.about.Location = new System.Drawing.Point(0, 327);
+            this.about.Name = "about";
+            this.about.Size = new System.Drawing.Size(507, 27);
+            this.about.TabIndex = 10;
+            this.about.Text = "About";
+            this.about.UseVisualStyleBackColor = true;
+            this.about.Click += new System.EventHandler(this.about_Click);
+            // 
+            // obs_spacecode
+            // 
+            this.obs_spacecode.AutoSize = true;
+            this.obs_spacecode.Location = new System.Drawing.Point(109, 19);
+            this.obs_spacecode.Name = "obs_spacecode";
+            this.obs_spacecode.Size = new System.Drawing.Size(85, 17);
+            this.obs_spacecode.TabIndex = 11;
+            this.obs_spacecode.Text = "Space Code";
+            this.obs_spacecode.UseVisualStyleBackColor = true;
+            this.obs_spacecode.CheckedChanged += new System.EventHandler(this.obs_spacecode_CheckedChanged);
+            // 
+            // obs_removecomments
+            // 
+            this.obs_removecomments.AutoSize = true;
+            this.obs_removecomments.Location = new System.Drawing.Point(6, 42);
+            this.obs_removecomments.Name = "obs_removecomments";
+            this.obs_removecomments.Size = new System.Drawing.Size(99, 17);
+            this.obs_removecomments.TabIndex = 12;
+            this.obs_removecomments.Text = "Strip Comments";
+            this.obs_removecomments.UseVisualStyleBackColor = true;
+            // 
+            // obs_commentspam
+            // 
+            this.obs_commentspam.AutoSize = true;
+            this.obs_commentspam.Location = new System.Drawing.Point(6, 65);
+            this.obs_commentspam.Name = "obs_commentspam";
+            this.obs_commentspam.Size = new System.Drawing.Size(100, 17);
+            this.obs_commentspam.TabIndex = 13;
+            this.obs_commentspam.Text = "Comment Spam";
+            this.obs_commentspam.UseVisualStyleBackColor = true;
+            // 
+            // obs_lowratio
+            // 
+            this.obs_lowratio.AutoSize = true;
+            this.obs_lowratio.Location = new System.Drawing.Point(109, 42);
+            this.obs_lowratio.Name = "obs_lowratio";
+            this.obs_lowratio.Size = new System.Drawing.Size(99, 17);
+            this.obs_lowratio.TabIndex = 14;
+            this.obs_lowratio.Text = "Low OBS Ratio";
+            this.obs_lowratio.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.obs_striplines);
+            this.groupBox1.Controls.Add(this.obs_lowratio);
+            this.groupBox1.Controls.Add(this.obs_removecomments);
+            this.groupBox1.Controls.Add(this.obs_spacecode);
+            this.groupBox1.Controls.Add(this.obs_strings);
+            this.groupBox1.Controls.Add(this.obs_hexfunctions);
+            this.groupBox1.Controls.Add(this.obs_commentspam);
+            this.groupBox1.Controls.Add(this.obs_variables);
+            this.groupBox1.Location = new System.Drawing.Point(12, 360);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(483, 106);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Obsfucation Settings";
+            // 
+            // main_window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 632);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.about);
+            this.Controls.Add(this.obsfucation_status);
+            this.Controls.Add(this.obsfucate_lua);
+            this.Controls.Add(this.ob_text);
+            this.Controls.Add(this.not_ob_text);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "main_window";
+            this.Text = "GLua-Obsfucator";
+            this.Load += new System.EventHandler(this.main_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox not_ob_text;
+        private System.Windows.Forms.RichTextBox ob_text;
+        private System.Windows.Forms.Button obsfucate_lua;
+        private System.Windows.Forms.ListBox obsfucation_status;
+        private System.Windows.Forms.CheckBox obs_striplines;
+        private System.Windows.Forms.CheckBox obs_variables;
+        private System.Windows.Forms.CheckBox obs_strings;
+        private System.Windows.Forms.CheckBox obs_hexfunctions;
+        private System.Windows.Forms.Button about;
+        private System.Windows.Forms.CheckBox obs_spacecode;
+        private System.Windows.Forms.CheckBox obs_removecomments;
+        private System.Windows.Forms.CheckBox obs_commentspam;
+        private System.Windows.Forms.CheckBox obs_lowratio;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ main_window.cs
@@ -0,0 +1,283 @@
+﻿/*
+    /////////////////////////////////////
+    // GLUA OBSFUCATOR                 //
+    // By Senator                      //
+    /////////////////////////////////////
+    // Project Started: 05-08-2016     //
+    // Project Ended: -/-/-            //
+    /////////////////////////////////////
+*/
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace glua_obsfucator
+{
+    public partial class main_window : Form
+    {
+        public main_window()
+        {
+            InitializeComponent();
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+            //MessageBox.Show("PRODUCT NOTICE\n\nthis Is freeware. If you paid any amount of money for this program\nrefund it as you were just scammed.", "NOTICE", MessageBoxButtons.OK);
+            MessageBox.Show(
+                "PRODUCT NOTICE\n\nThis is merely a project.\n not a professional application, expect bugs and issues.\nits an obsfucator, they are not perfect.",
+                "NOTICE", MessageBoxButtons.OK);
+        }
+
+        //  Credits to Original Source: http://stackoverflow.com/questions/16999604/convert-string-to-hex-string-in-c-sharp
+        private string StringToHex(string hexstring)
+        {
+            var sb = new StringBuilder();
+            foreach (var t in hexstring)
+            {
+                sb.Append("\\x" + Convert.ToInt32(t).ToString("x"));
+            }
+            return sb.ToString();
+        }
+
+        // Credits to Original Source: http://stackoverflow.com/questions/1344221/how-can-i-generate-random-alphanumeric-strings-in-c
+
+        public static string RandomString(int length)
+        {
+            const string chars = " {}#.*(%)!$ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        /// <summary>
+        /// Obsfucation Function Main
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void obsfucate_lua_Click(object sender, EventArgs e)
+        {
+            obsfucation_status.Items.Clear();
+
+            obsfucation_status.Items.Add("Obsfucation Start");
+            obsfucation_status.Items.Add("*****************");
+
+            // Obsfucation Start...
+            var obsfucated = "";
+            var nonobsfucated = not_ob_text.Text;
+            var iCount = 0;
+            var bSwitchCommentSpam = false;
+
+            // Lua Data Types
+            var LUA_VARIABLES = new string[] {}.ToList();
+            var LUA_FUNCTIONS = new string[] {}.ToList();
+
+            // Remove Comments
+            if (obs_removecomments.Checked)
+            {
+                nonobsfucated = new Regex(@"\/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+\/").Replace(nonobsfucated, "");
+                nonobsfucated = new Regex(@"\/\/.*").Replace(nonobsfucated, ""); // Remove // Comments
+            }
+
+            string line;
+            var strReader = new StringReader(nonobsfucated);
+            while (null != (line = strReader.ReadLine()))
+            {
+                var sLine = line;
+
+                // Type of line is a variable
+                var regex = new Regex(@"local.*="); // Detect if condition
+                var match = regex.Match(sLine);
+                if (match.Success && obs_variables.Checked)
+                {
+                    var sVariable = sLine;
+
+                    var rgx = new Regex("local."); // Remove local operator (if there is one)
+                    sVariable = rgx.Replace(sVariable, "");
+
+                    rgx = new Regex("=.*"); // Anything after = operator
+                    sVariable = rgx.Replace(sVariable, "");
+
+                    sVariable = sVariable.Replace(" ", ""); // Remove unwanted spaces for checking
+                    sVariable = sVariable.Replace("	", ""); // Remove unwanted spaces for checking
+
+                    obsfucation_status.Items.Add("LUA_VARIABLE '" + sVariable + "'");
+
+                    LUA_VARIABLES.Add(sVariable);
+                }
+
+                // Type of line is a function
+                regex = new Regex(@"function .*\(.*\)a*"); // Detect function condition
+                match = regex.Match(sLine);
+                if (match.Success & obs_variables.Checked)
+                {
+                    var sVariable = match.Value;
+
+                    if (!sVariable.Contains("function"))
+                        continue;
+
+                    var rgx = new Regex("local."); // Remove local operator (if there is one)
+                    sVariable = rgx.Replace(sVariable, "");
+
+                    rgx = new Regex("function."); // Remove Function operator
+                    sVariable = rgx.Replace(sVariable, "");
+
+                    rgx = new Regex(@"\(([^\)]+)\)"); // Bracket Operator
+                    sVariable = rgx.Replace(sVariable, "");
+
+                    sVariable = sVariable.Replace(" ", ""); // Remove unwanted spaces for checking
+
+                    obsfucation_status.Items.Add("LUA_FUNCTION '" + sVariable + "'");
+
+                    LUA_FUNCTIONS.Add(sVariable);
+                }
+
+                obsfucated += sLine +
+                              (obs_commentspam.Checked
+                                  ? " " + (bSwitchCommentSpam ? "/*" : "--[[") + " " +
+                                    RandomString(line.Length*(obs_lowratio.Checked ? 5 : 25)) +
+                                    " " + (bSwitchCommentSpam ? "*/" : "]]")
+                                  : "") +
+                              (obs_spacecode.Checked
+                                  ? new string('\n', line.Length) + new string(' ', line.Length)
+                                  : "\n");
+
+                bSwitchCommentSpam = !bSwitchCommentSpam;
+            }
+
+            // Random Seed
+            var rSeed = new Random();
+
+            // REPLACE ALL VARIABLES IN TABLE
+            foreach (var sCurBuffer in LUA_VARIABLES.ToArray())
+            {
+                iCount += 2;
+
+                var sRandom = new string('‬', iCount); // Create Random Length Variable
+                obsfucated = obsfucated.Replace(sCurBuffer, sRandom);
+
+                obsfucation_status.Items.Add("LUA_VARIABLES Obsfucated '" + sCurBuffer + "'");
+            }
+
+            // REPLACE ALL FUNCTIONS IN TABLE
+            foreach (var sCurBuffer in LUA_FUNCTIONS.ToArray())
+            {
+                iCount += 2;
+
+                var sRandom = new string('‬', iCount); // Create Random Length Variable
+                obsfucated = obsfucated.Replace(sCurBuffer, sRandom);
+
+                obsfucation_status.Items.Add("LUA_FUNCTIONS Obsfucated '" + sCurBuffer + "'");
+            }
+
+            //// Math Fuck
+            //var sMathFuck = new Regex(@"[=|==|+|-|/|*|(|)|[|\]|^]\s{1,}[0-9]{1,}").Matches(obsfucated); // Detect if variable is a number
+            //foreach (Match match in sMathFuck)
+            //{
+            //    var sMathString = match.Value;
+
+            //    var rgx = new Regex("local."); // Remove local operator (if there is one)
+            //    sVariable = rgx.Replace(sVariable, "");
+
+            //    var iEquation = int.Parse(sMathString);
+
+            //    MessageBox.Show(sMathString);
+            //}
+
+            // String Obsfucation
+            var sStrings = new Regex(@"""[^""\\]*(?:\\.[^""\\]*)*""").Matches(obsfucated);
+
+            // Detect Qouted Strings ("hello")
+            if (obs_strings.Checked)
+            {
+                foreach (Match match in sStrings)
+                {
+                    var sString = match.Value;
+
+                    sString = new Regex("^\"|\"$").Replace(sString, ""); // Strip Strings From Sentence
+
+                    obsfucated = obsfucated.Replace(match.Value, "\"" + StringToHex(sString) + "\"");
+                    // Obsfucate Sentence
+
+                    obsfucation_status.Items.Add("LUA_STRING Obsfucated '" + sString + "'");
+                }
+            }
+
+            // Replace Known Functions With obsfucation
+            var LUA_FUNCTIONHASHING = new string[] {}.ToList();
+
+            var file = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "function_hashes.dat");
+
+            var iConstructers = 0;
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line.Contains("//") || line.Length <= 1)
+                    continue;
+
+                iConstructers += 1;
+                LUA_FUNCTIONHASHING.Add(line);
+            }
+
+            file.Close();
+
+            obsfucation_status.Items.Add("function_hashes.dat Created " + iConstructers + " Function Hashes");
+
+            // Function Hashing
+            if (obs_hexfunctions.Checked)
+            {
+                foreach (var sCurBuffer in LUA_FUNCTIONHASHING.ToArray())
+                {
+                    var sFuncs = new Regex(sCurBuffer + @"\(([^\)]+)\)").Matches(obsfucated);
+                    // Detect Function-Style Comment
+                    foreach (Match match in sFuncs)
+                    {
+                        var sFunction = match.Value;
+                        var sParamaters = Regex.Match(sFunction, @"\(([^\)]+)\)").Value; // Get Paramaters From Function
+
+                        if (sFunction.Contains("."))
+                        {
+                            var sClassSplit = sFunction.Split('.');
+                            var sSendSegment = sClassSplit[1].Replace(sParamaters, "");
+
+                            obsfucated = obsfucated.Replace(sFunction,
+                                "getfenv()[\"" + StringToHex(sClassSplit[0]) + "\"]" + "[\"" + StringToHex(sSendSegment) +
+                                "\"]" + sParamaters); // Generate Obsfucated Function Call
+                        }
+                        else
+                        {
+                            obsfucated = obsfucated.Replace(sFunction,
+                                "getfenv()[\"" + StringToHex(sCurBuffer) + "\"]" + sParamaters);
+                            // Generate Obsfucated Function Call
+                        }
+
+                        obsfucation_status.Items.Add("LUA_FUNCTIONS Hashed '" + sCurBuffer + "'");
+                    }
+                }
+            }
+
+
+            if (obs_striplines.Checked)
+                obsfucated = obsfucated.Replace("\n", "");
+
+            ob_text.Text = obsfucated;
+
+            obsfucation_status.Items.Add("*****************");
+            obsfucation_status.Items.Add("Obsfucation End");
+        }
+
+        private void obs_spacecode_CheckedChanged(object sender, EventArgs e)
+        {
+            if (obs_spacecode.Checked)
+                obs_removecomments.Checked = true;
+        }
+
+        private void about_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "GLUA OBSFUCATOR\nBy Senator\n\nthanks To The Following:\n\n-Daz\n-Dark\n-Hollow\n-Phage\nAnyone else who contributed/idea's for the project!",
+                @"GLUA OBSFUCATOR");
+        }
+    }
+}
